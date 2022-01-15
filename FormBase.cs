53b651ed2db61a4f0bfa2716919263c433da4722
KarmaScripter <terry.eppler@gmail.com>
2022-01-15 12:56:08 -0500

    Added FormBase class;

---
+++ FormBase.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file = "FormBase.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
+// </copyright>
+//
+
+namespace BudgetExecution
+{
+    using System;
+    using Syncfusion.Windows.Forms;
+
+    public partial class FormBase : MetroForm
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="FormBase"/> class.
+        /// </summary>
+        public FormBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
