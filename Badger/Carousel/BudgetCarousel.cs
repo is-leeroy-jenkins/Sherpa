f98e4d710d9567ede95e2331592f07e22d1bbf7a
KarmaScripter <terry.eppler@gmail.com>
2022-02-09 11:26:17 -0500

    refactored Carousel constructor;

--- Badger/Carousel/BudgetCarousel.cs
+++ Badger/Carousel/BudgetCarousel.cs
@@ -21,7 +21,24 @@ namespace BudgetExecution
             Size = new Size( 300, 150 );
             CarouselPath = CarouselPath.Linear;
             ImageSlides = true;
-            
+            Padding = new Padding( 0, 0, 0, 0 );
+            PadX = 0;
+            PadY = 0;
+            Perspective = 4;
+            RotateAlways = false;
+            ShowImagePreview = true;
+            ShowImageShadow = true;
+            TransitionSpeed = 2;
+            UseOriginalImageinPreview = false;
+
+            // ThemeStyle Properties
+            ThemeStyle.BackColor = BudgetColor.Transparent;
+            ThemeStyle.Font = BudgetFont.FontSizeMedium;
+            ThemeStyle.ForeColor = BudgetColor.LightGray;
+            ThemeStyle.HoverImageBorderColor = BudgetColor.SteelBlue;
+            ThemeStyle.HoverImageBorderThickness = 2;
+            ThemeStyle.ImageShadeColor = Color.FromArgb( 64, 64, 64 );
+
             // Control Style Settings
             SetStyle( ControlStyles.OptimizedDoubleBuffer, true );
             SetStyle( ControlStyles.AllPaintingInWmPaint, true );
--- Badger/Test/TestForm.Designer.cs
+++ Badger/Test/TestForm.Designer.cs
@@ -56,6 +56,7 @@ namespace BudgetExecution
             this.carousel1.TabIndex = 1;
             this.carousel1.Text = "carousel1";
             this.carousel1.ThemeName = "Metro";
+            this.carousel1.ThemeStyle.BackColor = System.Drawing.Color.Transparent;
             this.carousel1.ThemeStyle.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.carousel1.ThemeStyle.ForeColor = System.Drawing.Color.LightGray;
             this.carousel1.ThemeStyle.HoverImageBorderColor = System.Drawing.Color.SteelBlue;
