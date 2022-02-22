4758d81b80158053a5d682004a1188a3d78b6535
KarmaScripter <terry.eppler@gmail.com>
2022-02-22 17:20:35 -0500

    added interfacce IBudgetListView

--- Badger/ListView/BudgetListView.cs
+++ Badger/ListView/BudgetListView.cs
@@ -16,7 +16,120 @@ namespace BudgetExecution
     using VisualPlus.Enumerators;
     using VisualPlus.Toolkit.Controls.Interactivity;
 
-    public class BudgetListView : ListViewBase
+    public interface IBudgetListView
+    {
+        /// <summary>
+        /// Gets or sets the binding source.
+        /// </summary>
+        /// <value>
+        /// The binding source.
+        /// </value>
+        BindingSource BindingSource { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tool tip.
+        /// </summary>
+        /// <value>
+        /// The tool tip.
+        /// </value>
+        ToolTip ToolTip { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hover text.
+        /// </summary>
+        /// <value>
+        /// The hover text.
+        /// </value>
+        string HoverText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the field.
+        /// </summary>
+        /// <value>
+        /// The field.
+        /// </value>
+        Field Field { get; set; }
+
+        /// <summary>
+        /// Gets or sets the numeric.
+        /// </summary>
+        /// <value>
+        /// The numeric.
+        /// </value>
+        Numeric Numeric { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filter.
+        /// </summary>
+        /// <value>
+        /// The filter.
+        /// </value>
+        IDictionary<string, object> DataFilter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bud ex configuration.
+        /// </summary>
+        /// <value>
+        /// The bud ex configuration.
+        /// </value>
+        NameValueCollection Setting { get; set; }
+
+        /// <summary>
+        /// Sets the color of the border.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        void SetBorderColor( Color color );
+
+        /// <summary>
+        /// Sets the color of the border.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        void SetHoverBorderColor( Color color );
+
+        /// <summary>
+        /// Sets the color of the border.
+        /// </summary>
+        /// <param name="hoverColor">if set to <c>true</c> [hover color].</param>
+        void SetBorderConfiguration( bool hoverColor );
+
+        /// <summary>
+        /// Sets the hover information.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        void SetHoverText( string text );
+
+        /// <summary>
+        /// Sets the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        void SetText( string text );
+
+        /// <summary>
+        /// Sets the tag.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        void ReTag( object tag );
+
+        /// <summary>
+        /// Adds the item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        void AddItem( string item );
+
+        /// <summary>
+        /// Adds the items.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        void AddItems( IEnumerable<string> items );
+
+        /// <summary>
+        /// Adds the item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        void RemoveItem( string item );
+    }
+
+    public class BudgetListView : ListViewBase, IBudgetListView
     {
         /// <summary>
         /// Gets or sets the binding source.
