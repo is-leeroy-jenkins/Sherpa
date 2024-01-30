07ffefc3b9811733e965081af18402c480c14963
KarmaScripter <terry.eppler@gmail.com>
2024-01-30 10:44:42 -0500

    update from rider rig;

--- Data/Builder/DataCalculator.cs
+++ Data/Builder/DataCalculator.cs
@@ -6,7 +6,7 @@
 //     Last Modified By:        Terry D. Eppler
 //     Last Modified On:        07-14-2023
 // ******************************************************************************************
-// <copyright file="MetricBase.cs" company="Terry D. Eppler">
+// <copyright file="DataCalculator.cs" company="Terry D. Eppler">
 //    This is a Federal Budget, Finance, and Accounting application for the
 //    US Environmental Protection Agency (US EPA).
 //    Copyright ©  2023  Terry Eppler
@@ -34,7 +34,7 @@
 //    You can contact me at:   terryeppler@gmail.com or eppler.terry@epa.gov
 // </copyright>
 // <summary>
-//   MetricBase.cs
+//   DataCalculator.cs
 // </summary>
 // ******************************************************************************************
 
