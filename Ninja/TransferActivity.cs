6b20b0445fb4dcbb76459c627acf5f2fd2b62702
KarmaScripter <terry.eppler@gmail.com>
2024-04-23 02:31:59 -0400

    up

--- Ninja/TransferActivity.cs
+++ Ninja/TransferActivity.cs
@@ -44,53 +44,167 @@ namespace BudgetExecution
     using System.Data;
     using System.Diagnostics.CodeAnalysis;
 
-    /// <summary> </summary>
+    /// <inheritdoc />
+    /// <summary>
+    /// </summary>
     [ SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" ) ]
+    [ SuppressMessage( "ReSharper", "UnusedType.Global" ) ]
+    [ SuppressMessage( "ReSharper", "RedundantBaseConstructorCall" ) ]
+    [ SuppressMessage( "ReSharper", "AssignNullToNotNullAttribute" ) ]
     public class TransferActivity : Transfer
     {
+        /// <inheritdoc />
         /// <summary>
         /// Initializes a new instance of the
-        /// <see cref="TransferActivity"/>
+        /// <see cref="T:BudgetExecution.TransferActivity" />
         /// class.
         /// </summary>
-        public TransferActivity( )
+        public TransferActivity( ) 
+            : base( )
         {
         }
 
+        /// <inheritdoc />
         /// <summary>
         /// Initializes a new instance of the
-        /// <see cref="TransferActivity"/>
+        /// <see cref="T:BudgetExecution.TransferActivity" />
         /// class.
         /// </summary>
         /// <param name="query"> The query. </param>
-        public TransferActivity( IQuery query )
+        public TransferActivity( IQuery query ) 
+            : base( query )
         {
-            Record = new DataBuilder( query ).Record;
-            Data = Record.ToDictionary( );
+            _record = new DataBuilder( query ).Record;
+            _id = int.Parse( _record[ "TransferActivityId" ]?.ToString( ) ?? "0" );
+            _bfy = _record[ "BFY" ]?.ToString( );
+            _efy = _record[ "EFY" ]?.ToString( );
+            _fundCode = _record[ "FundCode" ]?.ToString( );
+            _fundName = _record[ "FundName" ]?.ToString( );
+            _budgetLevel = _record[ "BudgetLevel" ]?.ToString( );
+            _rpioCode = _record[ "RpioCode" ]?.ToString( );
+            _rpioName = _record[ "RpioName" ]?.ToString( );
+            _ahCode = _record[ "AhCode" ]?.ToString( );
+            _ahName = _record[ "AhName" ]?.ToString( );
+            _orgCode = _record[ "OrgCode" ]?.ToString( );
+            _orgName = _record[ "OrgName" ]?.ToString( );
+            _accountCode = _record[ "AccountCode" ]?.ToString( );
+            _bocCode = _record[ "BocCode" ]?.ToString( );
+            _bocName = _record[ "BocName" ]?.ToString( );
+            _rcCode = _record[ "RcCode" ]?.ToString( );
+            _rcName = _record[ "RcName" ]?.ToString( );
+            _programProjectCode = _record[ "ProgramProjectCode" ]?.ToString( );
+            _programProjectName = _record[ "ProgramProjectName" ]?.ToString( );
+            _fromTo = _record[ "FromTo" ]?.ToString( );
+            _amount = double.Parse( _record[ "Amount" ]?.ToString( ) ?? "0" );
+            _resourceType = _record[ "ResourceType" ]?.ToString( );
+            _processedDate = DateOnly.Parse( _record[ "ProcessedDate" ]?.ToString( ) );
+            _quarter = _record[ "Quarter" ]?.ToString( );
+            _reprogrammingNumber = _record[ "ReprogrammingNumber" ]?.ToString( );
+            _controlNumber = _record[ "ControlNumber" ]?.ToString( );
+            _line = _record[ "Line" ]?.ToString( );
+            _subline = _record[ "Subline" ]?.ToString( );
+            _purpose = _record[ "Purpose" ]?.ToString( );
+            _bocCode = _record[ "BocCode" ]?.ToString( );
+            _bocName = _record[ "BocName" ]?.ToString( );
+            _programAreaCode = _record[ "ProgramAreaCode" ]?.ToString( );
+            _programAreaName = _record[ "ProgramAreaName" ]?.ToString( );
+            _npmCode = _record[ "NpmCode" ]?.ToString( );
+            _map = _record.ToDictionary( );
         }
 
+        /// <inheritdoc />
         /// <summary>
         /// Initializes a new instance of the
-        /// <see cref="TransferActivity"/>
+        /// <see cref="T:BudgetExecution.TransferActivity" />
         /// class.
         /// </summary>
         /// <param name="builder"> The builder. </param>
-        public TransferActivity( IDataModel builder )
+        public TransferActivity( IDataModel builder ) 
+            : base( builder )
         {
-            Record = builder.Record;
-            Data = Record.ToDictionary( );
+            _record = builder.Record;
+            _id = int.Parse( _record[ "TransferActivityId" ]?.ToString( ) ?? "0" );
+            _bfy = _record[ "BFY" ]?.ToString( );
+            _efy = _record[ "EFY" ]?.ToString( );
+            _fundCode = _record[ "FundCode" ]?.ToString( );
+            _fundName = _record[ "FundName" ]?.ToString( );
+            _budgetLevel = _record[ "BudgetLevel" ]?.ToString( );
+            _rpioCode = _record[ "RpioCode" ]?.ToString( );
+            _rpioName = _record[ "RpioName" ]?.ToString( );
+            _ahCode = _record[ "AhCode" ]?.ToString( );
+            _ahName = _record[ "AhName" ]?.ToString( );
+            _orgCode = _record[ "OrgCode" ]?.ToString( );
+            _orgName = _record[ "OrgName" ]?.ToString( );
+            _accountCode = _record[ "AccountCode" ]?.ToString( );
+            _bocCode = _record[ "BocCode" ]?.ToString( );
+            _bocName = _record[ "BocName" ]?.ToString( );
+            _rcCode = _record[ "RcCode" ]?.ToString( );
+            _rcName = _record[ "RcName" ]?.ToString( );
+            _programProjectCode = _record[ "ProgramProjectCode" ]?.ToString( );
+            _programProjectName = _record[ "ProgramProjectName" ]?.ToString( );
+            _fromTo = _record[ "FromTo" ]?.ToString( );
+            _amount = double.Parse( _record[ "Amount" ]?.ToString( ) ?? "0" );
+            _resourceType = _record[ "ResourceType" ]?.ToString( );
+            _processedDate = DateOnly.Parse( _record[ "ProcessedDate" ]?.ToString( ) );
+            _quarter = _record[ "Quarter" ]?.ToString( );
+            _reprogrammingNumber = _record[ "ReprogrammingNumber" ]?.ToString( );
+            _controlNumber = _record[ "ControlNumber" ]?.ToString( );
+            _line = _record[ "Line" ]?.ToString( );
+            _subline = _record[ "Subline" ]?.ToString( );
+            _purpose = _record[ "Purpose" ]?.ToString( );
+            _bocCode = _record[ "BocCode" ]?.ToString( );
+            _bocName = _record[ "BocName" ]?.ToString( );
+            _programAreaCode = _record[ "ProgramAreaCode" ]?.ToString( );
+            _programAreaName = _record[ "ProgramAreaName" ]?.ToString( );
+            _npmCode = _record[ "NpmCode" ]?.ToString( );
+            _map = _record.ToDictionary( );
         }
 
+        /// <inheritdoc />
         /// <summary>
         /// Initializes a new instance of the
-        /// <see cref="TransferActivity"/>
+        /// <see cref="T:BudgetExecution.TransferActivity" />
         /// class.
         /// </summary>
         /// <param name="dataRow"> The data row. </param>
         public TransferActivity( DataRow dataRow )
         {
-            Record = dataRow;
-            Data = dataRow.ToDictionary( );
+            _record = dataRow;
+            _id = int.Parse( dataRow[ "TransferActivityId" ]?.ToString( ) ?? "0" );
+            _bfy = dataRow[ "BFY" ]?.ToString( );
+            _efy = dataRow[ "EFY" ]?.ToString( );
+            _fundCode = dataRow[ "FundCode" ]?.ToString( );
+            _fundName = dataRow[ "FundName" ]?.ToString( );
+            _budgetLevel = dataRow[ "BudgetLevel" ]?.ToString( );
+            _rpioCode = dataRow[ "RpioCode" ]?.ToString( );
+            _rpioName = dataRow[ "RpioName" ]?.ToString( );
+            _ahCode = dataRow[ "AhCode" ]?.ToString( );
+            _ahName = dataRow[ "AhName" ]?.ToString( );
+            _orgCode = dataRow[ "OrgCode" ]?.ToString( );
+            _orgName = dataRow[ "OrgName" ]?.ToString( );
+            _accountCode = dataRow[ "AccountCode" ]?.ToString( );
+            _bocCode = dataRow[ "BocCode" ]?.ToString( );
+            _bocName = dataRow[ "BocName" ]?.ToString( );
+            _rcCode = dataRow[ "RcCode" ]?.ToString( );
+            _rcName = dataRow[ "RcName" ]?.ToString( );
+            _programProjectCode = dataRow[ "ProgramProjectCode" ]?.ToString( );
+            _programProjectName = dataRow[ "ProgramProjectName" ]?.ToString( );
+            _fromTo = dataRow[ "FromTo" ]?.ToString( );
+            _amount = double.Parse( dataRow[ "Amount" ]?.ToString( ) ?? "0" );
+            _resourceType = dataRow[ "ResourceType" ]?.ToString( );
+            _processedDate = DateOnly.Parse( dataRow[ "ProcessedDate" ]?.ToString( ) );
+            _quarter = dataRow[ "Quarter" ]?.ToString( );
+            _reprogrammingNumber = dataRow[ "ReprogrammingNumber" ]?.ToString( );
+            _controlNumber = dataRow[ "ControlNumber" ]?.ToString( );
+            _line = dataRow[ "Line" ]?.ToString( );
+            _subline = dataRow[ "Subline" ]?.ToString( );
+            _purpose = dataRow[ "Purpose" ]?.ToString( );
+            _bocCode = dataRow[ "BocCode" ]?.ToString( );
+            _bocName = dataRow[ "BocName" ]?.ToString( );
+            _programAreaCode = dataRow[ "ProgramAreaCode" ]?.ToString( );
+            _programAreaName = dataRow[ "ProgramAreaName" ]?.ToString( );
+            _npmCode = dataRow[ "NpmCode" ]?.ToString( );
+            _map = dataRow.ToDictionary( );
         }
     }
 }
